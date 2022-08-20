ce84b586c78bd79879495378cc195d74464e4d81
davidizac <davidgahnassia@gmail.com>
2022-08-20 22:31:31 +0300

    package.json

--- src/content/src/components/EditableElement/index.tsx
+++ src/content/src/components/EditableElement/index.tsx
@@ -12,6 +12,8 @@ import grapesjs from 'grapesjs'
 import customStyle from '!raw-loader!./style.css'
 import { useEffect, useRef, useState } from 'react'
 import 'grapesjs-blocks-basic'
+import 'grapesjs-rte-extensions/dist/grapesjs-rte-extensions.min.js'
+import iconPlus from '../../../assets/plus-icon.svg'
 
 export default function EditableElement({ top, left, htmlContent }) {
   const elementRef = useRef(null)
@@ -20,7 +22,7 @@ export default function EditableElement({ top, left, htmlContent }) {
     const editor = grapesjs.init({
       container: elementRef.current,
       fromElement: true,
-      height: '100px',
+      height: 'auto',
       // Disable the storage manager for the moment
       width: '100%',
       panels: { defaults: [] },
@@ -30,29 +32,86 @@ export default function EditableElement({ top, left, htmlContent }) {
         appendTo: '#blocks',
       },
     })
-    editor.on('component:add', (b) => {
-      const size = document
-        .getElementsByClassName('gjs-editor-cont')[0]
-        .style.height.replace('px', '')
-      document.getElementsByClassName('gjs-editor-cont')[0].style.height = `${
-        parseInt(size) + b.views[0].el.clientHeight
-      }px`
+
+    const domc = editor.DomComponents
+
+    domc.addType('text', {
+      view: {
+        events: {
+          mouseover: 'onMouseOver',
+          mouseout: 'onMouseOut',
+        },
+
+        onRender() {
+          const plusIcon = document.createElement('div')
+          plusIcon.className = 'plus-icon'
+          plusIcon.innerHTML = iconPlus
+          plusIcon.style.display = 'none'
+          this.el.appendChild(plusIcon)
+        },
+
+        onMouseOver(ev) {
+          ev.stopPropagation()
+          this.el.style.border = '2px dashed #9997FF'
+          this.el.style.transition = 'opacity 0.7s ease-out'
+          this.el.style.backgroundColor = 'rgba(153, 151, 255, 0.1)'
+          const plusIcon = this.el.querySelectorAll('.plus-icon')[0]
+          plusIcon.style.display = 'block'
+        },
+        onMouseOut(ev) {
+          ev.stopPropagation()
+          this.el.style.border = 'none'
+          this.el.style.transition = 'none'
+          this.el.style.backgroundColor = 'inherit'
+          const plusIcon = this.el.querySelectorAll('.plus-icon')[0]
+          plusIcon.style.display = 'none'
+        },
+      },
     })
-    editor.on('component:clone', (b) => {
-      const size = document
-        .getElementsByClassName('gjs-editor-cont')[0]
-        .style.height.replace('px', '')
-      document.getElementsByClassName('gjs-editor-cont')[0].style.height = `${
-        parseInt(size) + b.em.views[0].el.clientHeight
-      }px`
+
+    const iframe = document.getElementsByClassName('gjs-frame')[0]
+
+    iframe.addEventListener('load', function () {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.contentDocument.body.style.backgroundColor = 'transparent'
+      editor.addStyle(
+        '[data-gjs-type="wrapper"]{min-height: 110px;min-width: 250px; overflow-x: hidden; background-color: white; width: fit-content; border-radius: 10px }',
+      )
+      editor.addStyle('.gjs-selected{outline: none!important; outline-offset: 0px!important}')
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
+      iframe.style.transition = 'none'
     })
-    editor.on('component:remove', (b) => {
-      const size = document
-        .getElementsByClassName('gjs-editor-cont')[0]
-        .style.height.replace('px', '')
-      document.getElementsByClassName('gjs-editor-cont')[0].style.height = `${
-        parseInt(size) - b.views[0].el.clientHeight
-      }px`
+    editor.on('change:changesCount', function (e) {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
+    })
+    editor.on('component:update', function (e) {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
+    })
+    editor.on('component:add', function (e) {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
+    })
+    editor.on('component:remove', function (e) {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
+    })
+    editor.on('canvas:drop', function (e) {
+      const elementRef =
+        iframe.contentWindow.document.querySelectorAll('[data-gjs-type=wrapper]')[0]
+      iframe.style.height = elementRef.scrollHeight + 'px'
+      iframe.style.width = elementRef.scrollWidth + 'px'
     })
   }, [])
   return (
@@ -61,8 +120,6 @@ export default function EditableElement({ top, left, htmlContent }) {
       style={{
         top,
         left,
-        width: '300px',
-        height: '200px',
         position: 'absolute',
       }}
     >
@@ -77,7 +134,7 @@ export default function EditableElement({ top, left, htmlContent }) {
           <div ref={elementRef} style={{ backgroundColor: 'white' }}></div>
         </div>
       </div>
-      <div id='blocks'></div>
+      <div id='blocks' style={{ position: 'fixed' }}></div>
     </div>
   )
 }
